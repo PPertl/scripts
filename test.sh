17e8c23b1f2955cd8308dd40580a4f309a5c9fb1
PPertl <pertl.peter1010@gmail.com>
2024-07-16 06:58:46 +0200

    fix

--- test.sh
+++ test.sh
@@ -12,7 +12,6 @@ PYTHON_PACKAGES=(
 
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
-    "https://github.com/taabata/LCM_Inpaint_Outpaint_Comfy"
     "https://github.com/sipherxyz/comfyui-art-venture"
     "https://github.com/storyicon/comfyui_segment_anything"
     "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
@@ -20,6 +19,11 @@ NODES=(
     "https://github.com/DataCTE/prompt_injection"
     "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
     "https://github.com/Fannovel16/comfyui_controlnet_aux"
+    "https://github.com/WASasquatch/was-node-suite-comfyui"
+    "https://github.com/palant/image-resize-comfyui"
+    "https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/taabata/LCM_Inpaint_Outpaint_Comfy"
+
 )
 
 IPADAPTER_MODELS=(
